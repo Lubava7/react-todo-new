63870372478f08d664e39a58040664f08450ac30
lubava <joheunsaram@mail.ru>
2022-03-25 15:58:57 +0300

    edit smth

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import PageDashboard from "./pages/PageDashboard/PageDashboard";
 import PageProject from "./pages/PageProject/PageProject";
-import Header from "./components/Header/Header";
+// import Header from "./components/Header/Header";
+import ActiveLastBreadcrumb from "./components/Header/Header";
 
 function App() {
   return (
     <div>
-      <Header />
+      <ActiveLastBreadcrumb />
       <Routes>
         <Route path="/" element={<PageDashboard />} />
         <Route path="/project" element={<PageProject />} />
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,46 @@
-import React from "react";
+// import React from "react";
 import { Link } from "react-router-dom";
+import * as React from "react";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+// import Link from "@mui/material/Link";
 
-function Header() {
+// function handleClick(event) {
+//   event.preventDefault();
+//   console.info("You clicked a breadcrumb.");
+// }
+
+export default function ActiveLastBreadcrumb() {
   return (
-    <div>
-      <nav class="nav">
-        <li class="nav-item">
-          <Link class="nav-link active" to="/">
-            Dashboard
-          </Link>
-        </li>
-        <li class="nav-item">
-          <Link class="nav-link disabled" to="/project">
-            Project
-          </Link>
-        </li>
-      </nav>
-    </div>
+    // <div role="presentation" onClick={handleClick}>
+    <Breadcrumbs aria-label="breadcrumb">
+      <Link className="link" aria-current="page" to="/">
+        Dashboard
+      </Link>
+      <Link className="link" to="/project">
+        Project
+      </Link>
+    </Breadcrumbs>
+    /* </div> */
   );
 }
 
-export default Header;
+// function Header() {
+//   return (
+//     <div>
+//       <nav class="nav">
+//         <li class="nav-item">
+//           <Link class="nav-link active" to="/">
+//             Dashboard
+//           </Link>
+//         </li>
+//         <li class="nav-item">
+//           <Link class="nav-link disabled" to="/project">
+//             Project
+//           </Link>
+//         </li>
+//       </nav>
+//     </div>
+//   );
+// }
+
+// export default Header;
