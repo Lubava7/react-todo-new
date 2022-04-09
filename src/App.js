63e52f8349c7500bbbbc4477541c1ad1e3354ec7
lubava <joheunsaram@mail.ru>
2022-04-09 21:07:39 +0300

    edit App js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AddIcon from "@mui/icons-material/Add";
 
 function App(toggleTask) {
   const [projects, setProjects] = React.useState([]);
-  const [text, setText] = React.useState([]);
+  const [text, setText] = React.useState("");
   // const [todos, setTodos] = React.useState([]);
   const [counter, setCounter] = React.useState([]);
 
@@ -13,7 +13,7 @@ function App(toggleTask) {
     if (text) {
       const newProject = {
         id: Math.random().toString(36).substring(2, 9),
-        task: text,
+        name: text,
         // complete: false,
       };
       setProjects([...projects, newProject]);
@@ -43,19 +43,22 @@ function App(toggleTask) {
     <div className="TodoList">
       <div>Add a new Project:</div>
       <div>Amount of projects : {counter}</div>
-      <form onSubmit={handleSubmit}>
-        <button onClick={() => addProjects()} className="button-add-icon">
-          <AddIcon style={{ fontSize: "small" }} />
-          <input
-            className="input"
-            value={text}
-            type="text"
-            onChange={handleChange}
-            onKeyDown={handleKeyPress}
-            placeholder="create a project"
-          />
-        </button>
-      </form>
+
+      <button className="button-add-icon">
+        <AddIcon
+          style={{ fontSize: "small" }}
+          onClick={() => addProjects(text)}
+        />
+        <input
+          className="input"
+          value={text}
+          type="text"
+          onChange={handleChange}
+          onKeyDown={handleKeyPress}
+          placeholder="create a project"
+        />
+      </button>
+
       {projects.map((project) => {
         return (
           <div>
@@ -63,7 +66,7 @@ function App(toggleTask) {
               onClick={() => toggleTask(project.id)}
               className={project.complete ? "item-text strike" : "item-text"}
             >
-              {project.task}
+              {project.name}
               <div className="trash-check">
                 <div
                   className="item-delete"
@@ -80,50 +83,3 @@ function App(toggleTask) {
   );
 }
 export default App;
-
-//   deleteFriend = (userIndex, friendIndex) => {
-//     const currUser = { ...this.state.users[userIndex] };
-//     currUser.friends.splice(friendIndex, 1);
-//     const newArr = [...this.state.users];
-//     newArr[userIndex] = currUser;
-//     this.setState({ users: newArr });
-//   };
-//   addFriend = (userIndex, friends) => {
-//     const ui = { ...this.state.users[userIndex] };
-//     ui.friends.push({ name: this.state.text });
-//     const newArray = [...this.state.users];
-//     newArray[userIndex] = ui;
-//     this.setState({ users: newArray });
-//   };
-//   handleChange = (e) => {
-//     this.setState({ text: e.target.value });
-//   };
-
-//   render() {
-//     return (
-//       <>
-//         {this.state.users.map((user, userIndex) => {
-//           return (
-//             <div>
-//               {user.name}
-//               {user.age}
-//               {user.friends.map((friend, friendIndex) => {
-//                 return (
-//                   <p>
-//                     Friend: {friend.name}{" "}
-//                     <span
-//                       onClick={() => this.deleteFriend(userIndex, friendIndex)}
-//                     >
-//                       X
-//                     </span>
-//                   </p>
-//                 );
-//               })}
-//             </div>
-//           );
-//         })}
-
-//       </>
-//     );
-//   }
-// }
